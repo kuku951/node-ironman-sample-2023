5f2695d639588cbe940af6433c0aafc564d36837
Kay <kay101084@gmail.com>
2025-02-25 02:59:00 +0800

    period

--- routes/index.js
+++ routes/index.js
@@ -9,29 +9,27 @@ const ecpay_payment = require('ecpay_aio_nodejs');
 const { MERCHANTID, HASHKEY, HASHIV, HOST } = process.env;
 
 // SDK 提供的範例，初始化
-// https://github.com/ECPay/ECPayAIO_Node.js/blob/master/ECPAY_Payment_node_js/conf/config-example.js
 const options = {
-  OperationMode: 'Test', //Test or Production
+  OperationMode: 'Test', // Test or Production
   MercProfile: {
     MerchantID: MERCHANTID,
     HashKey: HASHKEY,
     HashIV: HASHIV,
   },
   IgnorePayment: [
-    //    "Credit",
-    //    "WebATM",
-    //    "ATM",
-        "CVS",
-        "BARCODE",
-        "AndroidPay"
+    "WebATM",
+    "ATM",
+    "CVS",
+    "BARCODE",
+    "AndroidPay"
   ],
   IsProjectContractor: false,
 };
+
 let TradeNo;
 
-router.get('/', (req, res) => {
-  // SDK 提供的範例，參數設定
-  // https://github.com/ECPay/ECPayAIO_Node.js/blob/master/ECPAY_Payment_node_js/conf/config-example.js
+// 定期定額信用卡付款
+router.get('/period', (req, res) => {
   const MerchantTradeDate = new Date().toLocaleString('zh-TW', {
     year: 'numeric',
     month: '2-digit',
@@ -42,24 +40,32 @@ router.get('/', (req, res) => {
     hour12: false,
     timeZone: 'UTC',
   });
-  TradeNo = 'test' + new Date().getTime();
+  TradeNo = 'period' + new Date().getTime();
+  
   let base_param = {
-    MerchantTradeNo: TradeNo, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
+    MerchantTradeNo: TradeNo,
     MerchantTradeDate,
-    TotalAmount: '100',
-    TradeDesc: '測試交易描述',
-    ItemName: '測試商品等',
+    TotalAmount: '50',
+    TradeDesc: '定期定額測試交易',
+    ItemName: '定期定額測試商品',
     ReturnURL: `${HOST}/return`,
     ClientBackURL: `${HOST}/clientReturn`,
   };
-  const create = new ecpay_payment(options);
 
-  // 注意：在此事直接提供 html + js 直接觸發的範例，直接從前端觸發付款行為
-  const html = create.payment_client.aio_check_out_all(base_param);
+  let period_params = {
+    PeriodAmount: '50',
+    PeriodType: 'M', // M: 月，D: 天，Y: 年
+    Frequency: '1', // 執行頻率
+    ExecTimes: '2', // 總執行次數
+    PeriodReturnURL: `${HOST}/return`,
+  };
+  
+  const create = new ecpay_payment(options);
+  const html = create.payment_client.aio_check_out_credit_period(period_params, base_param);
   console.log(html);
 
   res.render('index', {
-    title: 'Express',
+    title: 'Express - 定期定額',
     html,
   });
 });
@@ -67,10 +73,10 @@ router.get('/', (req, res) => {
 // 後端接收綠界回傳的資料
 router.post('/return', async (req, res) => {
   console.log('req.body:', req.body);
-
+  
   const { CheckMacValue } = req.body;
   const data = { ...req.body };
-  delete data.CheckMacValue; // 此段不驗證
+  delete data.CheckMacValue;
 
   const create = new ecpay_payment(options);
   const checkValue = create.payment_client.helper.gen_chk_mac_value(data);
@@ -82,7 +88,6 @@ router.post('/return', async (req, res) => {
     checkValue,
   );
 
-  // 交易成功後，需要回傳 1|OK 給綠界
   res.send('1|OK');
 });
 
